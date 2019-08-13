ed03a97a175238bfaea167050368d36a1c8f710d
Екатерина Бакалдина <e.bakaldina@city-mobil.ru>
2019-08-13 09:12:07 +0300

    first commit

---
+++ components/HeaderBanner.vue
@@ -0,0 +1,23 @@
+<template>
+    <main class="header-banner">
+        <div class="header-banner-item"></div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: "HeaderBaner"
+    }
+</script>
+
+<style scoped>
+    .header-banner-item {
+        background-image: url('~static/banner-1.png');
+        background-size: contain;
+        width: 100%;
+        height: 500px;
+    }
+    .header-banner {
+        margin-top: 128px;
+    }
+</style>
---
+++ components/HeaderComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-layout align-center justify-space-between row>
+        <div class="header-item logo">
+            <a href="#">
+                <img
+                        src="/logo.svg"
+                        alt="Ситимобил"
+                        class=""
+                >
+            </a>
+        </div>
+        <div class="header-item phone headline font-weight-medium">
+            <a href="tel:74955005050" class="black--text">+7 (495) 500 50 50</a>
+        </div>
+        <div class="header-item app">
+            <a target="_blank" href="https://itunes.apple.com/app/id579220388">
+                <img
+                        src="/appStore.svg"
+                        alt="App Store"
+                        class=""
+                >
+            </a>
+            <a target="_blank" href="https://play.google.com/store/apps/details?id=com.citymobil&referrer=mt_click_id%3Dmt-wnl9g1-1559827598-2186467004">
+                <img
+                        src="/googlePlay.svg"
+                        alt="Google Play"
+                        class=""
+                >
+            </a>
+        </div>
+    </v-layout>
+</template>
+
+<style>
+    .app a img{
+        width: 122px;
+    }
+</style>
